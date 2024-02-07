57b4386c8c2b1f45b3a8f924de688a38b07d4b18
Selim Burak Karakaya <124041205+selimburakkarakaya@users.noreply.github.com>
2024-02-07 22:09:31 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,45 @@
+import image1 from './images/1.webp';
+import image2 from './images/2.webp';
+import image3 from './images/3.webp';
+import image4 from './images/4.jpg';
+import image5 from './images/5.webp';
+import image6 from './images/6.webp';
+
+export const data = [
+    {
+        name: "Dark Green Sweatshirt",
+        price: 99.99,
+        image: image1,
+        quantity: 1
+    },
+    {
+        name: "Red Sweatshirt",
+        price: 98.99,
+        image: image2,
+        quantity: 1
+    },
+    {
+        name: "Purple Sweatshirt",
+        price: 109.99,
+        image: image3,
+        quantity: 1
+    },
+    {
+        name: "Beige Sweatshirt",
+        price: 89.99,
+        image: image4,
+        quantity: 1
+    },
+    {
+        name: "Blue Sweatshirt",
+        price: 129.99,
+        image: image5,
+        quantity: 1
+    },
+    {
+        name: "Green Sweatshirt",
+        price: 99.99,
+        image: image6,
+        quantity: 1
+    },
+];
